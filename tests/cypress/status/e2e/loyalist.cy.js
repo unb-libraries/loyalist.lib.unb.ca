f82399c45932c4847ef716a0a3f6b379afc03518
Patrick Schilf <pschilf@unb.ca>
2022-08-15 15:00:27 -0300

    TESTING-1 Migrate loyalist

---
+++ tests/cypress/status/e2e/loyalist.cy.js
@@ -0,0 +1,23 @@
+const host = 'https://loyalist.lib.unb.ca'
+describe('The Loyalist Collection', {baseUrl: host, groups: ['sites']}, () => {
+
+  describe('Front page', {baseUrl: host}, () => {
+    beforeEach(() => {
+      cy.visit('/')
+      cy.title()
+        .should('contain', 'The Loyalist Collection')
+    })
+
+    specify('Search for "Archives of Ontario" should find 7+ results', () => {
+      cy.get('.content form').within(() => {
+        cy.get('input[name="search_api_fulltext"]')
+          .type('Archives of Ontario')
+      }).submit()
+      cy.url()
+        .should('match', /\/loyalist-search/)
+      cy.get('.views-row')
+        .should('have.lengthOf.at.least', 7)
+    });
+  })
+
+})

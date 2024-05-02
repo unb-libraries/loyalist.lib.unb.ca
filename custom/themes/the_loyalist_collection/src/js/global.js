fa97a64ba26ac9f8b0e708f9902ce2b7a286e874
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-05-02 12:14:57 -0300

    LOYALIST-57 Move scss to src folder, add selectize library/etc assets

---
+++ custom/themes/the_loyalist_collection/src/js/global.js
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * Global lib_unb_ca JS functions.
+ */
+(function($, Drupal) {
+    'use strict';
+
+    Drupal.behaviors.loyalist_global = {
+        attach: function (context, settings) {
+            // JS feature code.
+        }
+    };
+})(jQuery, Drupal);

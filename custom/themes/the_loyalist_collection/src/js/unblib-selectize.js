88a56556ba0646eaa5cbdca0cfc32a5efdb5a70c
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-07-11 09:36:41 -0300

    Config catchup (WIP Views/Search Index/Carousel/etc)

--- custom/themes/the_loyalist_collection/src/js/unblib-selectize.js
+++ custom/themes/the_loyalist_collection/src/js/unblib-selectize.js
@@ -2,24 +2,17 @@
  * @file
  * UNB Libraries selectize.js initialization.
  */
-(function($, Drupal) {
-    'use strict';
-
-    Drupal.behaviors.unblib_selectize = {
-        attach: function () {
-            $('#edit-creator').selectize({
-                maxItems: null,
-                plugins: ['clear_button'],
-                selectOnTab: false,
-            });
-            $('select#edit-collection').selectize({
-                maxItems: null,
-                plugins: ["clear_button"],
-                selectOnTab: false,
-            });
-
-            // Button color change. TO DO: custom module hook_form_alter().
-            $('#edit-actions input:nth-child(2)').toggleClass('btn-primary btn-light border');
-        }
-    };
-})(jQuery, Drupal);
+(function ($) {
+    $(document).ready(function() {
+        $('#edit-creator').selectize({
+            maxItems: null,
+            plugins: ['clear_button'],
+            selectOnTab: false,
+        });
+        $('select#edit-collection').selectize({
+            maxItems: null,
+            plugins: ["clear_button"],
+            selectOnTab: false,
+        });
+    });
+})(jQuery);

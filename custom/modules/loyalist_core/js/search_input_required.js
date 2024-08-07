8a1eda1089184be02389308f6ea1d828e6d60466
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-08-07 13:40:53 -0300

    LOYALIST-20 Search results: add required feature w/ BS5+ally validity

---
+++ custom/modules/loyalist_core/js/search_input_required.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Add required attribute on input elements of type 'search'.
+ * Use built-in HTML5 form validation reporting and apply BS4 failure style on failure.
+ */
+(function ($) {
+    // Ensure required attribute present input of type 'search'.
+    const search_input = 'input[type="search"]';
+    const required = $(search_input).attr('required');
+    if (typeof required === 'undefined' && required !== true) {
+        $(search_input).attr('required', true);
+        $(search_input).attr('required', true);
+    }
+
+    // If form validity check fails then apply BS5 invalid class.
+    document.getElementById("edit-fulltext").addEventListener("invalid",
+        function () {
+                   $(this).addClass('is-invalid').attr('aria-invalid', 'true');
+        }
+    );
+
+})(jQuery);

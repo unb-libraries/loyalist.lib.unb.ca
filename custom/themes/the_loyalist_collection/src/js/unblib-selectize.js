a46e5da880b72cb3095f9700077751db568991b9
Jeremy McDermott <jeremy.mcdermott@gmail.com>
2024-11-26 13:20:54 -0400

    LOYALIST-56 Improved select input Selectize UX

--- custom/themes/the_loyalist_collection/src/js/unblib-selectize.js
+++ custom/themes/the_loyalist_collection/src/js/unblib-selectize.js
@@ -9,12 +9,14 @@
             maxItems: null,
             plugins: ['clear_button'],
             selectOnTab: false,
+            placeholder: '- All creators -',
         });
         // Section aka Collection Category aka Subject Heading.
         $('#edit-section').selectize({
             maxItems: null,
             plugins: ["clear_button"],
             selectOnTab: false,
+            placeholder: '- All categories -',
         });
         $('#edit-location').selectize({
             maxItems: null,
